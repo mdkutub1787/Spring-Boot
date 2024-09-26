711f6f7eb23f379925225fe8832ae41035deb484
Md Kutub Uddin <mdkutub150@gmail.com>
2024-09-26 13:11:06 +0600

    add

--- Spring Boot Withn Angular Security Part/InsuranceManagement/src/main/java/com/kutub/InsuranceManagement/entity/MarineInsuranceBill.java
+++ Spring Boot Withn Angular Security Part/InsuranceManagement/src/main/java/com/kutub/InsuranceManagement/entity/MarineInsuranceBill.java
@@ -24,17 +24,17 @@ public class MarineInsuranceBill {
     @Column( name = "warSrcc_rate_% ",nullable = false)
     private double warSrccRate;
 
-    @Column(nullable = false)
+//    @Column(nullable = false)
     private double netPremium;
 
     @Column( name = "tax_rate_% ",nullable = false)
-    private double tax;
+    private double tax = 15; // Fixed tax rate at 15%
 
 
     @Column(nullable = false)
     private double stampDuty;
 
-    @Column(nullable = false)
+//    @Column(nullable = false)
     private double grossPremium;
 
     @ManyToOne(fetch = FetchType.EAGER)

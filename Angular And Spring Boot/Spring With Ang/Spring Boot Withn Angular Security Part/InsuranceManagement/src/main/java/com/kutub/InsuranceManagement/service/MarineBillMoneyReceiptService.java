36a0d4009155533b077e6501aeee4e6eb6c2744b
Md Kutub Uddin <158472932+mdkutub1787@users.noreply.github.com>
2024-09-29 18:36:34 +0600

    Reapply "add"
    This reverts commit d4ba09f513e3d862d4df86ce12a83e999d651cae.

--- Angular And Spring Boot/Spring With Ang/Spring Boot Withn Angular Security Part/InsuranceManagement/src/main/java/com/kutub/InsuranceManagement/service/MarineBillMoneyReceiptService.java
+++ Angular And Spring Boot/Spring With Ang/Spring Boot Withn Angular Security Part/InsuranceManagement/src/main/java/com/kutub/InsuranceManagement/service/MarineBillMoneyReceiptService.java
@@ -37,7 +37,10 @@ public class MarineBillMoneyReceiptService {
         return marineBillMoneyReceiptRepo.findAll();
     }
 
-
+//    // Save a new marine bill money receipt
+//    public MarineBillMoneyReceipt saveMarineBillMoneyReceipt(MarineBillMoneyReceipt receipt) {
+//        return marineBillMoneyReceiptRepo.save(receipt); // Return the saved entity
+//    }
 
     // Find a marine bill money receipt by ID
     public MarineBillMoneyReceipt findById(long id) {
